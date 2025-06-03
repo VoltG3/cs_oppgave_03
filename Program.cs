ef1371e43e319c162770f0a06e0ce3d81f3f8fee
VoltG3 <kristiandanoss@gmail.com>
2025-06-03 23:57:48 +0200

    [ UPDATE ] - while

--- Program.cs
+++ Program.cs
@@ -13,41 +13,39 @@ class Program
         
         string[] expressionArray = preDefinedUserInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         
-        for (int i = 0; i < expressionArray.Length; i++)
+        while (expressionArray.Length > 1)
         {
             // get the next priority operator
             var opIndex = Operator.Index(expressionArray);
             
-            // target executable part of expression by priority operator
-            if (i == opIndex && i - 1 >= 0 && i + 1 < expressionArray.Length)
+            if (opIndex == -1 || opIndex <= 0 || opIndex >= expressionArray.Length - 1)
+                break;
+            
+            string left = expressionArray[opIndex - 1];
+            string op = expressionArray[opIndex];
+            string right = expressionArray[opIndex + 1];
+                
+            // execute part of the expression
+            Calculate calc = new Calculate();
+            string result = calc.Expression(left, op, right);
+                
+            // replace the part of the expression with the result
+            List<string> tempList = new List<string>();
+                
+            for (int j = 0; j < expressionArray.Length; j++)
             {
-                string left = expressionArray[i - 1];
-                string op = expressionArray[i];
-                string right = expressionArray[i + 1];
-                
-                // execute part of the expression
-                Calculate calc = new Calculate();
-                string result = calc.Expression(left, op, right);
-                
-                // replace the part of the expression with the result
-                List<string> tempList = new List<string>();
-                
-                for (int j = 0; j < expressionArray.Length; j++)
-                {
-                    if (j == opIndex - 1)
-                    {
-                        tempList.Add(result); 
-                    }
-                    
-                    if (j != opIndex && j != opIndex + 1 && j != opIndex - 1)
-                    {
-                        tempList.Add(expressionArray[j]);
-                    }
+                if (j == opIndex - 1) 
+                { 
+                    tempList.Add(result); 
                 }
-
-                expressionArray = tempList.ToArray();
+                
+                if (j != opIndex && j != opIndex + 1 && j != opIndex - 1)
+                { 
+                    tempList.Add(expressionArray[j]);
+                }
             }
-         
+
+            expressionArray = tempList.ToArray();
         }
         
         // ToDo -> print result, step by step
