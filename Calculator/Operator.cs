95649951917bf8f530d863c5bfe38b32252baee0
VoltG3 <kristiandanoss@gmail.com>
2025-06-03 22:50:46 +0200

    [ UPDATE ] - operator

--- Calculator/Operator.cs
+++ Calculator/Operator.cs
@@ -1,44 +1,23 @@
 namespace  cs_oppgave_03;
 
-// Return: Priority Operator Index
-//   Multiplication (*)
-//   Division (/)
-//   Addition (+) and
-//   Subtraction (-) 
-
 public class Operator
 {
     public static int Index(string[] arr)
     {
-        string charMul = "*"; bool boolMul = false;
-        string charDiv = "/"; bool boolDiv = false;
-        string charAdd = "+"; 
-        string charSub = "-"; 
-        
-        int targetPriorityOperatorIndex = 0;
-        
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] == charSub && targetPriorityOperatorIndex == 0)
-            {
-                targetPriorityOperatorIndex = i;
-            }
-            
-            else if (arr[i] == charAdd && boolMul == false && boolDiv == false)
-            {
-                boolMul = true;
-                targetPriorityOperatorIndex = i;
-            }
-            
-            else if (arr[i] == charDiv && boolDiv == false && boolDiv == false)
-            {
-                boolDiv = true;
-                targetPriorityOperatorIndex = i;
-            }
+        // Priority sequence: * / + -
+        string[] priority = new[] { "*", "/", "+", "-" };
 
-            // Mangler else
+        foreach (string op in priority)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == op)
+                {
+                    return i;
+                }
+            }
         }
         
-        return targetPriorityOperatorIndex;
+        return -1;
     }
 }
